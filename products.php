730d048a14fa0d7a5854252c137a156e2efd1f93
kemboi22 <85908528+kemboi22@users.noreply.github.com>
2022-06-03 22:26:17 +0300

    Cdn

--- products.php
+++ products.php
@@ -17,6 +17,9 @@ $operation = new operations();
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/product/">
 
 
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
 
     <!-- Bootstrap core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
@@ -120,6 +123,8 @@ $operation = new operations();
 
 <script src="./js/bootstrap.bundle.min.js"></script>
 <script src="jquery/jquery.js"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <script>
 
 </script>
